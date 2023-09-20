752ee82fcc0dc9c9359a1d194868e79d33afc5ca
Erick Cerna <erick.cerna@alignet.com>
2023-09-19 22:52:55 -0500

    pwa

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(),
     VitePWA({
       injectRegister: 'auto',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
