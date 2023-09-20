4825f59ab5a093cc853a40449275d4b762d2d2f1
Erick Cerna <erick.cerna@alignet.com>
2023-09-19 21:43:19 -0500

    pwa

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA(),
+    VitePWA({
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    }),
   ],
 })
